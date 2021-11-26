86e2466f887ec7b77ced8b65a5cc264814617e92
gdoor-isaque <isaque@gdoor.com.br>
2021-11-25 21:03:21 -0300

    usuarios online

--- server.js
+++ server.js
@@ -6,20 +6,43 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-io.on('connection', (socket) => {     
+var fs = require('fs');
+const { getUnpackedSettings } = require('http2');
+
+var users = {};
+
+io.on('connection', (socket) => {
 
     console.log('a user connected ' + socket.handshake.query.nick);
 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('user disconnect', socket.id)
+        delete users[socket.id];
+        io.emit('usersChange', users);
+
+        socket.broadcast.emit('user disconnect', { name: socket.handshake.query.nick, id: socket.id })
         console.log('user has disconnected')
     });
-    
-    socket.broadcast.emit('new user', socket.handshake.query.nick);
-    
+
+    users[socket.id] = {
+        name: socket.handshake.query.nick,
+        id: socket.id
+    }
+
+    io.emit('usersChange', users);
+
+    socket.broadcast.emit('new user', { name: socket.handshake.query.nick, id: socket.id });
+
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);        
-        io.emit('chat message', {'msg': msg, 'nick': socket.handshake.query.nick});
+
+        fs.appendFile('./messages/history.txt', `${Date()}: ${socket.handshake.query.nick}: ${msg} \n`, (erro) => {
+            if (erro) {
+                console.log(`ocorreu um erro ${erro}`);
+            } else {
+                console.log('mensagem gravada');
+            }
+        });
+        console.log('message: ' + msg);
+        io.emit('chat message', { 'msg': msg, 'nick': socket.handshake.query.nick });
     });
 });
 
